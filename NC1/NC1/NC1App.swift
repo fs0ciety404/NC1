8f17cbd6eff87786d1876acef4628d5ab16afe8c
fs0ciety404 <113516391+fs0ciety404@users.noreply.github.com>
2022-11-17 17:49:59 +0100

    update

---
+++ NC1/NC1/NC1App.swift
@@ -0,0 +1,27 @@
+//
+//  NC1App.swift
+//  NC1
+//
+//  Created by Davide Ragosta on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            TabView{
+                SerieTvView()
+                    .tabItem {
+                        Label("Serie Tv", systemImage: "tv")
+                    }
+                FilmView()
+                    .tabItem {
+                        Label("Film", systemImage: "film")
+                    }
+            }
+            
+        }
+    }
+}
---
+++ NC1/NC1/Utils.swift
@@ -0,0 +1,24 @@
+//
+//  Utilis.swift
+//  NC1
+//
+//  Created by Davide Ragosta on 17/11/22.
+//
+
+import Foundation
+
+class Utils {
+    
+    static let jsonDecoder: JSONDecoder = {
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        jsonDecoder.dateDecodingStrategy = .formatted(dateFormatter)
+        return jsonDecoder
+    }()
+    
+    static let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-mm-dd"
+        return dateFormatter
+    }()
+}
