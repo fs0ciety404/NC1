c55d12d5ab96f24c597e25f41ef4c706544183c6
fs0ciety404 <113516391+fs0ciety404@users.noreply.github.com>
2022-11-19 00:30:36 +0100

    bug fix

---
+++ NC1/NC1/ViewModel/SafariView.swift
@@ -0,0 +1,23 @@
+//
+//  SafariView.swift
+//  NC1
+//
+//  Created by Davide Ragosta on 19/11/22.
+//
+
+import SafariServices
+import SwiftUI
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {
+        
+    }
+    
+    func makeUIViewController(context: Context) -> SFSafariViewController {
+        let safariVC = SFSafariViewController(url: self.url)
+        return safariVC
+    }
+}
